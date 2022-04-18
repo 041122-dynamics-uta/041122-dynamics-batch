15dead5c653b020c3dbb5989ef27c0404c8049fc
Mark Moore <mark.moore@revature.com>
2022-04-18 13:17:22 -0500

    add demos folder

---
+++ demos/consoleAppdemo/helloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿// using System;
+
+
+// See https://aka.ms/new-console-template for more information
+using System;
+
+Console.WriteLine("Hello, Dynamics365 Batch!!\n");
+Console.Write("Hey");
+Console.Write("there,");
+Console.Write("Tiger");
+Console.Write("!!!!");
+
