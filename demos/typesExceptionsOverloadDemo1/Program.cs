6b5706393f5cb98758fa7e9119d13852b5c6725b
Mark Moore <mark.moore@revature.com>
2022-04-21 16:07:50 -0500

    adds datatypes, exceptions, method overloading demos.

---
+++ demos/typesExceptionsOverloadDemo1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace typesExceptionsOverloadDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
+//this is .NET 6.0
