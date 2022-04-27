4e3776821aac265175603dc412855ac40e5f6382
Mark Moore <mark.moore@revature.com>
2022-04-27 14:49:48 -0500

    adds serialization demo

---
+++ demos/serializationdemo/Human.cs
@@ -0,0 +1,71 @@
+namespace ClassesDemo
+{
+    public class Human : Animal, Ipilot
+    {
+        #region This is the properties region
+        public string FavoriteColor { get; set; } = "";
+        public string MiddleName { get; set; } = "";
+
+        public int PilotIdNumber { get; set; } = 0;
+        public DateTime DateOfCertification { get; set; } = DateTime.Now;
+        public string TypeOfPlane { get; set; } = "origami";
+        #endregion
+
+        #region This is the constructors region
+        public Human() { }// creatign this parameterless constructor because serialization requires a parameterless constructor
+
+        public Human(string fname, string lname, string favColor) : base(fname, lname)
+        {
+            this.FavoriteColor = favColor;
+        }
+
+        public Human(string fname, string lname, string mname, string favColor) : base(fname, lname)
+        {
+            this.FavoriteColor = favColor;
+            this.MiddleName = mname;
+        }
+        #endregion
+
+        #region This region is for Methods
+
+        public void AnimalAge(int age)
+        {
+            this.MyAnimalAge.AnimalAge = age;
+        }
+
+        internal override string FullName()
+        {
+            string fullName = $"{this.Fname} {this.MiddleName} {this.Lname}";
+            return fullName;
+        }
+
+        //  I 'explicitely' implemented the interface methods (using 
+        // their full name, i.e., Ipilot.Greeting()), therefore they could not be 
+        public string Greeting()
+        {
+            //throw new NotImplementedException();
+            return "Welcome to the Friendly Skies. Where a single bird means yo u end up in the Hudson. Happy Flying!";
+        }
+
+        /// <summary>
+        /// This method will calculate the amount of days since the DateOfCertification adn return a sentence
+        /// including the pilots name and number of says.
+        /// </summary>
+        /// <returns></returns>
+        public string TimeSinceCertification()
+        {
+            //throw new NotImplementedException();
+            return "";
+        }
+
+        /// <summary>
+        /// This method will take a datetime and store is at a mm/dd/yyyy DateTime.
+        /// </summary>
+        /// <param name="d"></param>
+        public void SetDateOfCertification(DateTime d)
+        {
+            //throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
---
+++ demos/serializationdemo/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Xml.Serialization;
+using System.IO;
+using ClassesDemo;
+
+//an argument (arg) is the data sent with a call to a method
+Animal a1 = new Animal("George", "Washington", 25);
+
+// a1 is an instance of an animal class. a1 is an object
+string a1Fname = a1.Fname;
+
+Console.WriteLine($"The animal is named {a1Fname} and it's {a1.AnimalAge()} years old.");
+MyStaticClass.MarksAddress = "1234 Main st. Fort Worth TX.";
+
+Console.WriteLine($"Marks addy is {MyStaticClass.MarksAddress}.");
+
+
+Human h1 = new Human("Alexander", "Hamilton", "blue");
+
+Console.WriteLine($"This young whipper snapper, {h1.Fname} {h1.Lname}, wants a {h1.FavoriteColor} revolution!");
+
+Human h2 = new Human("Thomas", "Jefferson", "Raise a Glass", "Red");
+
+Console.WriteLine($"This young whipper snapper, {h2.Fname} {h2.MiddleName} {h2.Lname}, wants a {h2.FavoriteColor} revolution!");
+
+h2.AnimalAge(30);
+Console.WriteLine($"The age of {h2.FullName()} is {h2.AnimalAge()}");
+
+Human h3 = new Human("Sully", "Sullenberger", "Slimy Green");
+
+h3.SetDateOfCertification(new DateTime(2000, 1, 1));
+
+// Step 1. create a serializetr from the System.Xml.Serialization class
+List<Human> hulist = new List<Human> { h1, h2, h3 };// we can declare and instantiate a list along with objects at the same time!
+hulist.Add(h1);
+XmlSerializer serializer1 = new XmlSerializer(hulist.GetType());
+using (FileStream stream = File.OpenWrite("humanStorage.xml")) // this is a using block. It is different from the using declaration at the top fo the page.
+{
+    serializer1.Serialize(stream, hulist);
+}
+
+//File.AppendAllText("humanStorage.xml", hulist);
+
+List<Human> huinlist = new List<Human>();
+using (FileStream instream = File.OpenRead("humanStorage.xml"))
+{
+    hulist = (List<Human>)serializer1.Deserialize(instream);
+}
+
+foreach (Human h in huinlist)
+{
+    Console.WriteLine($"THe human is {h.FullName()} and it's favorite color is {h.FavoriteColor}");
+}
+
+
