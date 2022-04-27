225e2292990ff1213239c51a50a0a4deb72ad13f
Mark Moore <mark.moore@revature.com>
2022-04-27 11:18:08 -0500

    adds inteface demo.

--- demos/ClassesDemo/Human.cs
+++ demos/ClassesDemo/Human.cs
@@ -1,9 +1,12 @@
 namespace ClassesDemo
 {
-    internal class Human : Animal
+    internal class Human : Animal, Ipilot
     {
         public string FavoriteColor { get; set; } = "";
         public string MiddleName { get; set; } = "";
+        int Ipilot.PilotIdNumber { get; set; } = 0;
+        DateTime Ipilot.DateOfCertification { get; set; } = DateTime.Now;
+        string Ipilot.TypeOfPlane { get; set; } = "origami";
 
         public Human(string fname, string lname, string favColor) : base(fname, lname)
         {
@@ -30,6 +33,31 @@ namespace ClassesDemo
             return fullName;
         }
 
+        string Ipilot.Greeting()
+        {
+            //throw new NotImplementedException();
+            return "Welcome to the Friendly Skies. Where a single bird means yo u end up in the Hudson. Happy Flying!";
+        }
 
+        /// <summary>
+        /// This method will calculate the amount of days since the DateOfCertification adn return a sentence
+        /// including the pilots name and number of says.
+        /// </summary>
+        /// <returns></returns>
+        string Ipilot.TimeSinceCertification()
+        {
+            //throw new NotImplementedException();
+            return "";
+        }
+
+        /// <summary>
+        /// This method will take a datetime and store is at a mm/dd/yyyy DateTime.
+        /// </summary>
+        /// <param name="d"></param>
+        internal void Ipilot.SetDateOfCertification(DateTime d)
+        {
+            //throw new NotImplementedException();
+
+        }
     }
 }
