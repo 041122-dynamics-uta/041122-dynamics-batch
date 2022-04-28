81b8a93b8d6edc45304da9f49f0f1e2e853a7def
Mark Moore <mark.moore@revature.com>
2022-04-28 16:21:52 -0500

    adds TDD/xUnit testing

---
+++ demos/loggingTddDemoAll/LoggingTddDemo/Human.cs
@@ -0,0 +1,119 @@
+using System;
+using loggingTdddemo;
+
+namespace ClassesDemo
+{
+    public class Human : Animal, Ipilot
+    {
+        #region This is the properties region
+        public string FavoriteColor { get; set; } = "";
+        public string MiddleName { get; set; } = "";
+
+        public int PilotIdNumber { get; set; } = 0;
+        public DateTime DateOfCertification { get; set; } = DateTime.Now;
+        public string TypeOfPlane { get; set; } = "origami";
+
+        public LoggerClass _logger { get; set; }
+        #endregion
+
+        #region This is the constructors region
+        public Human()
+        {
+            this._logger = new LoggerClass();
+        }// creating this parameterless constructor because serialization requires a parameterless constructor
+
+        public Human(string fname, string lname, string favColor) : base(fname, lname)
+        {
+            this.FavoriteColor = favColor;
+            this._logger = new LoggerClass();
+        }
+
+        public Human(string fname, string lname, string mname, string favColor) : base(fname, lname)
+        {
+            this.FavoriteColor = favColor;
+            this.MiddleName = mname;
+            this._logger = new LoggerClass();
+        }
+        #endregion
+
+        #region This region is for Methods
+
+        public void LogThatHuman()
+        {
+            this._logger.CreatedHuman(this);
+        }
+
+        public void AnimalAge(int age)
+        {
+            this.MyAnimalAge.AnimalAge = age;
+            this._logger.HumanAgeSet(this, age);
+        }
+
+        public bool TryAnimalAge(int age)
+        {
+            if (age > 0 && age < 120)
+            {
+                this.MyAnimalAge.AnimalAge = age;
+                this._logger.HumanAgeSet(this, age);
+                return true;
+            }
+            return false;
+        }
+
+        internal override string FullName()
+        {
+            string fullName = $"{this.Fname} {this.MiddleName} {this.Lname}";
+            return fullName;
+        }
+
+        //  I 'explicitely' implemented the interface methods (using 
+        // their full name, i.e., Ipilot.Greeting()), therefore they could not be 
+        public string Greeting()
+        {
+            //throw new NotImplementedException();
+            return "Welcome to the Friendly Skies. Where a single bird means yo u end up in the Hudson. Happy Flying!";
+        }
+
+        /// <summary>
+        /// This method will calculate the amount of days since the DateOfCertification adn return a sentence
+        /// including the pilots name and number of says.
+        /// </summary>
+        /// <returns></returns>
+        public string TimeSinceCertification()
+        {
+            //throw new NotImplementedException();
+            return "";
+        }
+
+        /// <summary>
+        /// This method will take a datetime and store is at a mm/dd/yyyy DateTime.
+        /// </summary>
+        /// <param name="d"></param>
+        public void SetDateOfCertification(DateTime d)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public bool ChangeThisBool(bool b)
+        {
+            b = !b;
+            return b;
+        }
+
+        public int ChangeThisInt(int b)
+        {
+            b = ++b;
+            return b;
+        }
+
+
+        public Human ChangeThisName(Human h /*0x56fba*/)
+        {
+            h/*0x56fba*/.Fname = "Bradley";
+            h/*0x56fba*/.Lname = "Cooper";
+            return h/*0x56fba*/;
+        }
+
+        #endregion
+    }
+}
---
+++ demos/loggingTddDemoAll/LoggingTddDemo/Program.cs
@@ -0,0 +1,104 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Xml.Serialization;
+using System.IO;
+using ClassesDemo;
+using loggingTdddemo;
+using System.Collections.Generic;
+
+LoggerClass _logger = new LoggerClass();
+
+//an argument (arg) is the data sent with a call to a method
+Animal a1 = new Animal("George", "Washington", 25);
+
+// a1 is an instance of an animal class. a1 is an object
+string a1Fname = a1.Fname;
+
+Console.WriteLine($"The animal is named {a1Fname} and it's {a1.AnimalAge()} years old.");
+MyStaticClass.MarksAddress = "1234 Main st. Fort Worth TX.";
+
+Console.WriteLine($"Marks addy is {MyStaticClass.MarksAddress}.");
+
+
+Human h1 = new Human("Alexander", "Hamilton", "blue");
+//logg that humna
+h1.LogThatHuman();
+
+Console.WriteLine($"This young whipper snapper, {h1.Fname} {h1.Lname}, wants a {h1.FavoriteColor} revolution!");
+
+Human h2 = new Human("Thomas", "Jefferson", "Raise a Glass", "Red");
+h2.LogThatHuman();
+
+Console.WriteLine($"This young whipper snapper, {h2.Fname} {h2.MiddleName} {h2.Lname}, wants a {h2.FavoriteColor} revolution!");
+
+h2.AnimalAge(30);
+Console.WriteLine($"The age of {h2.FullName()} is {h2.AnimalAge()}");
+
+Human h3 = new Human("Sully", "Sullenberger", "Slimy Green");
+h3.LogThatHuman();
+
+h3.SetDateOfCertification(new DateTime(2000, 1, 1));
+
+
+
+/* The stack (16 gb RAM)
+...is a small memory storage help physically close to the processor/
+Therefore, it is fast to access. Programs you are working on are copied to the stack
+any local variables you declare in your code are also on the stack IF the coding language stores them there.
+
+When a key and value are both help on the stack, the data type is called VALUE TYPE.
+
+
+/*The HEAP (ex 1Tb memory)
+...is a BIG memory that is also a litttle slow. It is physicaly further away from the processor(s).
+It stores you applications while you aren't using them.
+In C#, and other high level coding languages, certain Types are always help on the HEAP. 
+These types are called REFERENCE TYPES.
+*/
+
+
+
+// What is a value Type?
+// int, char (string Type "Mark"),
+string m = "Mark";// a string is held on the HEAP
+Console.WriteLine(m);
+
+bool myBool = true;// a boolean is a value type.
+Console.WriteLine("Original value of mybool..." + myBool);
+// myBool = false;
+// Console.WriteLine(myBool);
+
+bool c = h3.ChangeThisBool(myBool);// send mybool into the method
+Console.WriteLine("Value of myBool after being changed in the method.." + myBool); // write the value of the boolean
+Console.WriteLine("Value returned from the method..." + c);// write the value of the return from the method.
+
+//now with an int!
+int myInt = 111;// a boolean is a value type.
+Console.WriteLine("Original value of myInt..." + myInt);
+// myBool = false;
+// Console.WriteLine(myBool);
+
+myInt = h3.ChangeThisInt(myInt);// send mybool into the method
+Console.WriteLine("Value of myInt after being changed in the method.." + myInt); // write the value of the boolean
+Console.WriteLine("Value returned from the method..." + myInt);// write the value of the return from the method.
+
+
+//take h3 and send it to ChangeThisName();
+Human h4 /*0x56fba*/ = h3.ChangeThisName(h3/*0x56fba*/);
+Console.WriteLine("Value of h3 after being changed in the method.." + h3.FullName()); // write the value of the boolean
+Console.WriteLine("Value of h4 returned from the method..." + h4.FullName());// write the value of the return from the method.
+
+
+Console.WriteLine("Fav Color of h3" + h3.FavoriteColor); // write the value of the boolean
+h3.FavoriteColor = "fucia";
+Console.WriteLine("Value of h4 returned from the method..." + h4.FavoriteColor);// write the value of the return from the method.
+
+
+List<int> intList = new List<int> { 4, 6, 3, 8, 2, 9, 1, 90 };
+
+intList.Sort();
+foreach (int x in intList)
+{
+    Console.WriteLine($"x is {x}");
+}
+
